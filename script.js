bdf0dfec1b20e23254f38ca0936d3a40bc80781a
Gustosa <gustavomartins5820@gmail.com>
2024-02-20 19:46:16 -0300

    Adicionar função de selecionar jogada do usuário

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function getComputerChoice() {
     // Math.random() retorna um número >= 0 e < 1, e multiplicando por 3, pode-se chegar a números maiores que 2. Assim, é possível que o Math.floor() retorne até 2.
-    let computerChoice = Math.floor((Math.random() * 3));
+    const computerChoice = Math.floor((Math.random() * 3));
     let result;
 
     switch (computerChoice) {
@@ -23,8 +23,6 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
     let roundMessage;
 
-    playerSelection = playerSelection.toLowerCase();
-
     if (playerSelection === computerSelection) {
         result = `It's a tie! Both played ${playerSelection}`;
     }
@@ -40,4 +38,18 @@ function playRound(playerSelection, computerSelection) {
     }
 
     return roundMessage;
+}
+
+function getUserChoice() {
+    let userChoice = prompt("This is a rock-paper-scissors game! Choose a move to play against the computer.");
+
+    userChoice = userChoice.toLowerCase();
+    console.log(userChoice)
+
+    if (userChoice === "rock" || userChoice === "paper" || userChoice === "scissors") {
+        return userChoice;
+    } else {
+        alert("Please, enter a valid move (either rock, paper or scissors).");
+        getUserChoice();
+    }
 }
