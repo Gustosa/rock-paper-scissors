719dc36c25da4e304bc12f341f11792f72144715
Gustosa <gustavomartins5820@gmail.com>
2024-05-18 17:49:19 -0300

    Adicionar UI para o jogo

--- script.js
+++ script.js
@@ -1,93 +1,78 @@
 function getComputerChoice() {
     // Math.random() retorna um número >= 0 e < 1, e multiplicando por 3, pode-se chegar a números maiores que 2. Assim, é possível que o Math.floor() retorne até 2.
-    const computerChoice = Math.floor((Math.random() * 3));
-    let result;
+    const computerChoice = Math.floor((Math.random() * 3))
+    let result
 
     switch (computerChoice) {
         case 0:
-            result = "rock";
-            break;
+            result = "rock"
+            break
         case 1:
-            result = "paper";
+            result = "paper"
             break;
         case 2:
-            result = "scissors";
-            break;
+            result = "scissors"
+            break
         default:
-            console.log(`Something is wrong. The returned number was ${computerChoice}.`);
+            console.log(`Something is wrong. The returned number was ${computerChoice}.`)
     }
 
     return result;
 }
 
-function getPlayerChoice() {
-    let userChoice = prompt("This is a rock-paper-scissors game! You will play a best of 5 against the computer. Choose a move to play against the computer.");
+function updateScore(roundResult) {
+    const userScore = document.querySelector("#userScore")
+    const userPoints = parseInt(userScore.innerText) + 1
+    const computerScore = document.querySelector("#computerScore")
+    const computerPoints = parseInt(computerScore.innerText) + 1
+    const finalResult = document.querySelector("#finalResult")
 
-    userChoice = userChoice.toLowerCase();
+    if (roundResult == "won") {
+        userScore.innerText = userPoints
+    } else if (roundResult == "lost") {
+        computerScore.innerText = computerPoints
+    }
 
-    if (userChoice === "rock" || userChoice === "paper" || userChoice === "scissors") {
-        return userChoice;
-    } else {
-        alert("Please, enter a valid move (either rock, paper or scissors).");
-        getPlayerChoice();
+    if (userPoints == 5 || computerPoints == 5) {
+        finalResult.innerText = `You won ${userPoints} rounds and lost ${computerPoints}.`
+
+        if (userPoints == 5) {
+            finalResult.innerText += " Congratulations! You won the set!"
+        }
+
+        if (computerPoints == 5) {
+            finalResult.innerText += " That sucks! The computer won this set."
+        }
+
+        userScore.innerText = "0"
+        computerScore.innerText = "0"
     }
 }
 
-function playRound(computerSelection, playerSelection) {
+function playRound(event) {
+    const playerSelection = event.target.id
+    const computerSelection = getComputerChoice()
+    const result = document.querySelector("#roundResult")
     let roundResult;
 
     if (playerSelection === computerSelection) {
-        result = `It's a tie! Both played ${playerSelection}`;
-    }
-
-    if (playerSelection !== computerSelection) {
+        result.innerText = `It's a tie! Both played ${playerSelection}.`
+    } else if (playerSelection !== computerSelection) {
         if (playerSelection === "rock" && computerSelection === "paper" ||
             playerSelection === "paper" && computerSelection === "scissors" ||
             playerSelection === "scissors" && computerSelection === "rock") {
-            roundResult = "defeat";
-            console.log(`You lost! The computer's ${computerSelection} beated your ${playerSelection}.`);
+            roundResult = "lost"
+            result.innerText = `You lost! The computer's ${computerSelection} beated your ${playerSelection}.`
         } else {
-            console.log(`You won! Your ${playerSelection} beated the computer's ${computerSelection}.`);
-            roundResult = "win";
+            roundResult = "won"
+            result.innerText = `You won! Your ${playerSelection} beated the computer's ${computerSelection}.`
         }
     }
 
-    return roundResult;
+    updateScore(roundResult)
 }
 
-function playGame() {
-    let result;
 
-    let defeatCounter = 0;
-    let winCounter = 0;
 
-    let computerSelection;
-    let playerSelection;
-
-    while (winCounter < 3 && defeatCounter < 3) {
-        computerSelection = getComputerChoice();
-        playerSelection = getPlayerChoice();
-
-        roundResult = playRound(computerSelection, playerSelection);
-
-        if (roundResult === "defeat") {
-            defeatCounter++;
-        }
-
-        if (roundResult === "win") {
-            winCounter++;
-        }
-    }
-
-    if (defeatCounter === 3) {
-        result = `You lost! Total wins: ${winCounter}`;
-    }
-
-    if (winCounter === 3) {
-        result = `You won! Hurray! Total losses: ${defeatCounter}`;
-    }
-
-    return result;
-}
-
-console.log(playGame());
+const options = document.querySelector("#options")
+options.addEventListener("click", playRound)
